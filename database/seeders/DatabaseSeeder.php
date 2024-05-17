fcb7ba73b77aaabcb539bfdd79f4b4220c633827
Muhammad Khaidar Royani <mkhaidar168@gmail.com>
2024-05-17 21:11:25 +0700

    clone project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,8 +28,8 @@ class DatabaseSeeder extends Seeder
         );
         User::create(
             [
-                'name' => 'Arif Budiman Arrosyid',
-                'email' => 'arifbudimanarrosyid@gmail.com',
+                'name' => 'Muhammad Khaidar Royani',
+                'email' => 'mkhaidar168@gmail.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
